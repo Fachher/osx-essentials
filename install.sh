1c0fe9c216ea92991ccd3ef3ed9749d4d2388372
fachher.syed <fachher@googlemail.com>
2019-02-11 22:02:33 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+
+if [ ! -f /usr/local/bin/brew ]; then
+	echo "Installing brew."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+if [ ! -f /usr/local/bin/brew ]; then
+	echo "Brew is required but missing !!!"
+	exit 1
+fi
+
+brew tap wagoodman/dive
+brew install dive
+brew install the_silver_searcher
+brew install neovim
+brew install gcc-arm-none-eabi
+brew install wine
+brew install mingw-w64
+brew install python-neovim
+brew install ctags
+brew install yarn
+brew install openocd
+brew install weechat
+brew install mc
+brew install htop
+brew install wireshark
